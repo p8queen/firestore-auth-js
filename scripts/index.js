22f1f3d0b81055335ae7abb6b26b5872841358f5
Gustavo Carmona <p8queen@gmail.com>
2023-03-07 22:06:09 -0300

    part 10
    login security

--- scripts/index.js
+++ scripts/index.js
@@ -4,20 +4,26 @@ const guideList = document.querySelector('.guides');
 // setup guides
 const setupGuides = (data) => {
 
-  let html = '';
-  data.forEach(doc => {
-    const guide = doc.data();
-    const li = `
-      <li>
-        <div class="collapsible-header grey lighten-4"> ${guide.title} </div>
-        <div class="collapsible-body white"> ${guide.content} </div>
-      </li>
-    `;
-    html += li;
-  });
-  guideList.innerHTML = html
+  if(data.length) {
+    let html = '';
+    data.forEach(doc => {
+      const guide = doc.data();
+      const li = `
+        <li>
+          <div class="collapsible-header grey lighten-4"> ${guide.title} </div>
+          <div class="collapsible-body white"> ${guide.content} </div>
+        </li>
+      `;
+      html += li;
+    });
+    guideList.innerHTML = html
+  }else{
+    guideList.innerHTML = '<h5 class="center-align">Login to view the content</h5>';
+  }
 
-};
+  };
+
+  
 
 // setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
